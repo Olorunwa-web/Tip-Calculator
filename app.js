f5e2ae67fd932c18ea0d3628addf70c8a6e05ff2
olorunwa-web <olorunwamoliki@gmail.com>
2025-02-12 16:10:58 +0100

    first commit

--- app.js
+++ app.js
@@ -13,14 +13,31 @@
 
 let tipPercentage = 0;
 let buttonss = document.querySelectorAll('.percent button');
+let customTipInput = document.getElementById('custTip')
 
 function setTip(percent) {
     tipPercentage = percent;
     buttonss.forEach(btn => btn.classList.remove('active'));
     buttonss[[5, 10, 15, 25, 50].indexOf(percent)].classList.add('active')
+    document.getElementById('custTip').value = '';
     calculateTip();
 }
 
+// function customTip() {
+//     let customTipChange  = document.getElementById('custTip').value
+//     if (customTipChange) {
+//         tipPercentage = parseFloat(customTipChange);
+//         buttonss.forEach(btn => btn.classList.remove('active'));
+//         calculateTip()
+//     }
+// }
+
+customTipInput.addEventListener('input', function () {
+    tipPercentage = parseFloat(this.value) || 0;
+    buttonss.forEach(btn => btn.classList.remove('active'));
+    this.classList.add('active')
+    calculateTip()
+})
 
 function calculateTip() {
     let input1 = parseFloat(document.getElementById('billInput').value);
@@ -29,7 +46,7 @@ function calculateTip() {
     let tips = document.getElementById('total-price');
     let Reset = document.querySelector('.reset-btn')
 
-    if ( input1 > 0  &&  input2 > 0) {
+    if ( input1 > 0  &&  input2 > 0  && tipPercentage >= 0) {
         let tipAmount = ( input1 * (tipPercentage / 100)) / input2;
         let totalAmount = ( input1 / input2) + tipAmount;
 
@@ -39,6 +56,8 @@ function calculateTip() {
     }
   
 }
+
+
 
 
 function validationPeople() {
@@ -61,7 +80,7 @@ function validationPeople() {
 function resetAll() {
     document.getElementById('billInput').value = '';
     document.getElementById('peopleInput').value = '';
-    document.getElementById('cost').value = '';
+    document.getElementById('custTip').value = '';
     document.getElementById('tip-price').innerText = '0.00';
     document.getElementById('total-price').innerText = '0.00';
     document.querySelector('.reset-btn').disabled = true;
